6bc4328f21f28b6c8beea32ceafa90bcb64f8974
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-11-11 12:39:50 -0600

    Add Test Order

--- AutomationTraining_M7/AutomationTraining_M7/Test Cases/Prueba1.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Test Cases/Prueba1.cs
@@ -12,21 +12,21 @@ namespace AutomationTraining_M7.Test_Cases
 {
     class Prueba1 : BaseTest 
     {
-        [Test]
+        [Test, Order(0)]
         public void TestLogin1()
         {
             FnSendkeyAndClear(By.Name("userName"), ConfigurationManager.AppSettings.Get("username"));
             FnSendkeyAndClear(By.Name("password"), ConfigurationManager.AppSettings.Get("password"));
         }
 
-        [Test]
+        [Test, Order(1)]
         public void TestLogin2()
         {
             FnSendkeyAndClear(By.XPath("//*[@name='userName']"), ConfigurationManager.AppSettings.Get("username2"));
             FnSendkeyAndClear(By.XPath("//*[@name='password']"), ConfigurationManager.AppSettings.Get("password2"));
         }
 
-        [Test]
+        [Test, Order(2)]
         public void GetElemenHrefList()
         {
             IList<IWebElement> ElementList = driver.FindElements(By.XPath("//a"));
