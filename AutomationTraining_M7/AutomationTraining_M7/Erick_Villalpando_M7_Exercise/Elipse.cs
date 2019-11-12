d8cc4607ede379323020af62b1deb7da2ff2d4b7
evillalpando1 <47833077+evillalpando1@users.noreply.github.com>
2019-11-12 09:55:47 -0600

    #rd commit

--- AutomationTraining_M7/AutomationTraining_M7/Erick_Villalpando_M7_Exercise/Elipse.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Erick_Villalpando_M7_Exercise/Elipse.cs
@@ -8,7 +8,36 @@ namespace AutomationTraining_M7.Erick_Villalpando_M7_Exercise
 {
     public class Elipse:_2D_Shape
     {
-        
+            int a;
+            int b;
+            //declaramos el constructor
+            public Elipse(int x, int y)
+            {
+                a = x;
+                b = y;
+            }
+            public int Area()
+            {
+                return a + b;
+            }
+
+            public int Perimeter()
+            {
+                return a - b;
+            }
+
+            class Principal
+            {
+                static void Main(string[] args)
+                {
+                //creamos objeto de la clase y le pasamos los parametros al constructor
+                    Elipse obj = new Elipse(10, 20);
+                    Console.WriteLine("Area of sphire is: " + obj.Area());
+                    Console.WriteLine("Perimeter is: " + obj.Perimeter());
+                    Console.ReadKey();
+                }
+            }
+        }
 
     }
 }
