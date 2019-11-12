87bd1db8b63c9233982a71916b0a4d46c940a9ec
JuanLannister <juan.lopez@4thsource.com>
2019-11-12 17:34:27 -0600

    Juan Lopez

---
+++ AutomationTraining_M7/AutomationTraining_M7/Juan_Lopez_M7_Excercise/IShape.cs
@@ -0,0 +1,11 @@
+﻿namespace AutomationTraining_M7.Juan_Lopez_M7_Excercise
+{
+    internal interface IShape
+    {
+        double Area();
+        double Perimetro();
+        double Volumen();
+        void DisplayInfo();
+        void Shape_Set();
+    }
+}
---
+++ AutomationTraining_M7/AutomationTraining_M7/Juan_Lopez_M7_Excercise/Shape_3D.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AutomationTraining_M7.Juan_Lopez_M7_Excercise
+{
+    class Shape_3D : Shape_2D
+    {
+        public double Volumen
+        {
+            get { return (Anchura * Base1 * Altura * (3.1416 * 4 / 3)); }
+        }
+        public double Anchura;
+        
+        public new void DisplayInfo() 
+        {
+            Console.Out.WriteLine("Area:" + Area);
+            Console.Out.WriteLine("Perimetro:" + Perimetro);
+            Console.Out.WriteLine("Volumen:" + Volumen);
+        }
+        public void Shape_3D_Set()
+        {
+            Shape_2D_Set();
+            Anchura = 0;
+        }
+        public double VolumenEllipsoid() 
+        {
+            return Area* Perimetro* Altura*(3.1416*4/3);
+        }
+    }
+}
