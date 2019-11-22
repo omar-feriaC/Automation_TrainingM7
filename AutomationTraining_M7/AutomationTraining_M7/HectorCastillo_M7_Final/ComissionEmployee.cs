c7aa28b4c01206a8b04f42d93b15d3b48a6bf294
Hector Castillo <hector.castillo@4thsource.com>
2019-11-21 19:00:47 -0600

    excercise dont calculete payroll

--- AutomationTraining_M7/AutomationTraining_M7/HectorCastillo_M7_Final/ComissionEmployee.cs
+++ AutomationTraining_M7/AutomationTraining_M7/HectorCastillo_M7_Final/ComissionEmployee.cs
@@ -8,5 +8,39 @@ namespace AutomationTraining_M7.HectorCastillo_M7_Final
 {
     class ComissionEmployee : SalaryEmployee
     {
+        //Atributes
+        public int intId { get; set; }
+        public string strName { get; set; }
+        public double dblPayroll { get; set; }
+        public int intCommission { get; set; }
+
+        public ComissionEmployee()
+        {
+            intId = 0;
+            strName = "undefined";
+            intCommission = 0;
+            dblPayroll = 0;
+        }
+
+        public ComissionEmployee(int pintId, string pstrName, double pdblPayroll, int pintCommission)
+        {
+            intId = pintId;
+            this.strName = pstrName;
+            dblPayroll = pdblPayroll;
+            intCommission = pintCommission;
+        }
+
+        public void fnCalculatePayroll()
+        {
+            dblPayroll = dblPayroll + intCommission;
+        }
+
+        public void fnDisplayInfo()
+        {
+            Console.WriteLine("Id: " + intId);
+            Console.WriteLine("Name: " + this.strName);
+            Console.WriteLine("Payroll: " + dblPayroll);
+            Console.WriteLine("Payroll and Commission: " + dblPayroll);
+        }
     }
 }
--- AutomationTraining_M7/AutomationTraining_M7/Program.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Program.cs
@@ -8,12 +8,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AutomationTraining_M7
+namespace AutomationTraining_M7.HectorCastillo_M7_Final
 {
     class Program : BaseTest
     {
         static void Main(string[] args)
         {
+            SalaryEmployee objSalaryEmployee = new SalaryEmployee(1, "Hector", 20000);
+            objSalaryEmployee.fnDisplayInfo();
+
+            Console.WriteLine("*******************************");
+
+            Console.ReadKey();
+
+            HourlyEmployee objHourlyEmployee = new HourlyEmployee(2, "Pedro", 5, 2);
+            objHourlyEmployee.fnDisplayInfo();
+
+            Console.WriteLine("*******************************");
+
+            Console.ReadKey();
+
+            ComissionEmployee objComissionEmployee = new ComissionEmployee(3, "Juan", 2000, 5);
+            objComissionEmployee.fnDisplayInfo();
+
+            Console.WriteLine("*******************************");
+
             Console.ReadKey();
 
         }
