9343b72b7f57229e4fbcc3087fd1f5e427cf0a31
Mauricio Guillermo Xacur <mauricio.guillermo@4thsource.com>
2019-11-21 19:07:26 -0600

    changes

--- AutomationTraining_M7/AutomationTraining_M7/MauricioGuillermo_M7_Final/HourlyEmployee.cs
+++ AutomationTraining_M7/AutomationTraining_M7/MauricioGuillermo_M7_Final/HourlyEmployee.cs
@@ -8,10 +8,23 @@ namespace AutomationTraining_M7.MauricioGuillermo_M7_Final
 {
     class HourlyEmployee : IPayrollCalculator
     {
-        public int HoursWorked;
-        public int HourRate;
         public int intId { get; set; }
         public string strName { get; set; }
+        public int intHoursWorked { get; set; }
+        public int intHourRate { get; set; }
+        
+        public HourlyEmployee(int hId, string hName, int hHoursWorked, int hHoursRate)
+        {
+            intId = hId;
+            strName = hName;
+            intHoursWorked = hHoursWorked;
+            intHourRate = hHoursRate;
+        }
+
+        public int fnCalculatePayroll()
+        {
+            return intHoursWorked * intHourRate;
+        }
 
         public void fnDisplayInfo()
         {
