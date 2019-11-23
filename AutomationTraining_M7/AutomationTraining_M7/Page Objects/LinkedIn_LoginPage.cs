d934d94eef00194c328463a31be4a1f56e0001ef
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-11-23 06:35:44 -0600

    Linkedin Login Page

---
+++ AutomationTraining_M7/AutomationTraining_M7/Page Objects/LinkedIn_LoginPage.cs
@@ -0,0 +1,31 @@
+﻿using AutomationTraining_M7.Base_Files;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTraining_M7.Page_Objects
+{
+    class LinkedIn_LoginPage : BaseTest
+    {
+        /*DRIVER REFERENCE FOR POM*/
+        private static IWebDriver _objDriver;
+
+        /*LOCATORS FOR EACH ELEMENT*/
+        readonly static string STR_USERNAME_TEXT = "username";
+        readonly static string STR_PASSWORD_TEXT = "password";
+        readonly static string STR_SIGNIN_BTN = "//button[text()='Sign in']";
+        
+        /*CONSTRUCTOR*/
+        public LinkedIn_LoginPage(IWebDriver pobjDriver)
+        {
+            _objDriver = pobjDriver;
+        }
+
+        /*IWEBELEMEMT OBJECTS*/
+        private static IWebElement objUserName => _objDriver.FindElement(By.Id(""));
+
+    }
+}
