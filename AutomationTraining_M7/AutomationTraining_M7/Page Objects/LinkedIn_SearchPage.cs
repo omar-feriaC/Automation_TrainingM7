f1052d6d4ad379b9b95be87e34ca40161f1f3289
dluna86 <47647769+dluna86@users.noreply.github.com>
2019-12-01 18:41:27 -0600

    Weekend updates

--- AutomationTraining_M7/AutomationTraining_M7/Page Objects/LinkedIn_SearchPage.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Page Objects/LinkedIn_SearchPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutomationTraining_M7.Base_Files;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,128 @@ using System.Threading.Tasks;
 
 namespace AutomationTraining_M7.Page_Objects
 {
-    class LinkedIn_SearchPage
+    class LinkedIn_SearchPage : BaseTest
     {
+        /*DRIVER REFERENCE FOR POM*/
+        private static IWebDriver _ObjSrcDriver;
+
+        /*LOCATORS FOR EACH ELEMENT*/
+        readonly static string STR_SEARCH_TEXT = "//input[@placeholder='Search']";
+        readonly static string STR_SEARCH_BTN = "//input[@placeholder='Search']";
+        readonly static string STR_PEOPLE_BTN = "//button[span[text()='People' or text()='Gente']]";
+        readonly static string STR_ALLFILTERS_BTN = "//button[span[text()='All Filters' or text()='Todos los Filtros']]";
+        readonly static string STR_REGIONMX_CB = "//input[@id='sf-geoRegion-mx:0']";
+        readonly static string STR_LANG_ENG_CB = "//input[@id='sf-profileLanguage-en']";
+        readonly static string STR_LANG_ESP_CB = "//input[@id='sf-profileLanguage-es']";
+        readonly static string STR_APPLY_BTN = "//button[@data-control-name='all_filters_apply']";
+
+        /*CONSTRUCTOR*/
+        public LinkedIn_SearchPage(IWebDriver pobjSrcDriver) 
+        {
+            _ObjSrcDriver = pobjSrcDriver;
+        }
+
+        /*IWEBELEMEMT OBJECTS*/
+        private static IWebElement objSearchText => _ObjSrcDriver.FindElement(By.XPath(STR_SEARCH_TEXT));
+        private static IWebElement objSearchBtn => _ObjSrcDriver.FindElement(By.XPath(STR_SEARCH_BTN));
+        private static IWebElement objPeopleBtn => _ObjSrcDriver.FindElement(By.XPath(STR_PEOPLE_BTN));
+        private static IWebElement objAllFiltersBtn => _ObjSrcDriver.FindElement(By.XPath(STR_ALLFILTERS_BTN));
+        private static IWebElement objRegionMxCb => _ObjSrcDriver.FindElement(By.XPath(STR_REGIONMX_CB));
+        private static IWebElement objLangEngCb => _ObjSrcDriver.FindElement(By.XPath(STR_LANG_ENG_CB));
+        private static IWebElement objLangEspCb => _ObjSrcDriver.FindElement(By.XPath(STR_LANG_ESP_CB));
+        private static IWebElement objApplyBtn => _ObjSrcDriver.FindElement(By.XPath(STR_APPLY_BTN));
+
+        /*METHODS*/
+        //Search Txt
+        private IWebElement GetSearchField()
+        {
+            return objSearchText;
+        }
+
+        public static void fnEnterSearchText(string pstrSearchText)
+        {
+            objSearchText.Click();
+            objSearchText.Clear();
+            objSearchText.SendKeys(pstrSearchText);
+        }
+
+        //Search Button
+        private IWebElement GetSearchButton() 
+        {
+            return objSearchBtn;
+        }
+
+        public static void fnClickSearchBtn() 
+        {
+            objSearchBtn.Click();
+        }
+
+        //People Checkbox
+        private IWebElement GetPeopleCB() 
+        {
+            return objPeopleBtn;
+        }
+
+        public static void fnSelectPeople() 
+        {
+            objPeopleBtn.Click();
+        }
+
+        //All Filters button
+        private IWebElement GetAllFilters() 
+        {
+            return objAllFiltersBtn;
+        }
+
+        public static void fnSelectAllFilters() 
+        {
+            objAllFiltersBtn.Click();
+        }
+
+        //Location Mexico
+        private IWebElement GetRegionMx()
+        {
+            return objRegionMxCb;
+        }
+
+        public static void fnGetRegionMx()
+        {
+            objRegionMxCb.Click();
+        }
+
+        //Language English
+        private IWebElement GetLanguageEng()
+        {
+            return objLangEngCb;
+        }
+
+        public static void fnLanguageEng()
+        {
+            objLangEngCb.Click();
+        }
+
+        //Language Espanish
+        private IWebElement GetLanguageEsp()
+        {
+            return objLangEspCb;
+        }
+
+        public static void fnLanguageEsp()
+        {
+            objLangEspCb.Click();
+        }
+
+        //Apply the Filters
+        private IWebElement GetApplybutton()
+        {
+            return objApplyBtn;
+        }
+
+        public static void fnClickApplyBtn()
+        {
+            objApplyBtn.Click();
+        }
+
+
     }
 }
--- AutomationTraining_M7/AutomationTraining_M7/Test Cases/Test_LinkedInSearch.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Test Cases/Test_LinkedInSearch.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using AutomationTraining_M7.Base_Files;
+using AutomationTraining_M7.Page_Objects;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AutomationTraining_M7.Test_Cases
 {
-    class Test_LinkedInSearch
+    class Test_LinkedInSearch : BaseTest
     {
+        LinkedIn_LoginPage objLogin;
+        LinkedIn_SearchPage objSearch;
+
+        [Test]
+        public void Login_LinkedIn()
+        {
+            objLogin = new LinkedIn_LoginPage(driver);
+            Assert.AreEqual(true, driver.Title.Contains("Login"), "Title not mach");
+            LinkedIn_LoginPage.fnEnterUserName(ConfigurationManager.AppSettings.Get("username"));
+            LinkedIn_LoginPage.fnEnterPassword(ConfigurationManager.AppSettings.Get("password"));
+            LinkedIn_LoginPage.fnClickSignInButton();
+            Assert.AreEqual(true, driver.Title.Contains("feed"), "Title not mach");
+
+
+        }
+
+
     }
 }
