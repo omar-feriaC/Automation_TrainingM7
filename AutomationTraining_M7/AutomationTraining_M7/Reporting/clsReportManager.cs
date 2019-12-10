3a8eddf42c9e5207109c09ade339d0854f853905
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-12-09 18:11:49 -0600

    Add fnReportPath function

---
+++ AutomationTraining_M7/AutomationTraining_M7/Reporting/clsReportManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTraining_M7.Reporting
+{
+    class clsReportManager
+    {
+        public string fnReportPath()
+        {
+            var strPath = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            var strActualPath = strPath.Substring(0, strPath.LastIndexOf("bin"));
+            var strProjectPath = new Uri(strActualPath).LocalPath;
+            Directory.CreateDirectory(strProjectPath.ToString() + "ExtentReports");
+            var strReportPath = strProjectPath + "ExtentReports\\ExtentReports.html";
+            return strReportPath;
+        }
+
+
+    }
+}
