d8da98e7e7d2bc160514b9b254acbc29ca7f0785
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-12-16 13:55:41 -0600

    Commit M9Exercise Changes

--- AutomationTraining_M7/AutomationTraining_M7/Reporting/clsReportManager.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Reporting/clsReportManager.cs
@@ -16,7 +16,6 @@ namespace AutomationTraining_M7.Reporting
     class clsReportManager
     {
         private DateTime time = DateTime.Now;
-        private string strImagePath;
 
         public string fnReportPath()
         {
@@ -91,76 +90,6 @@ namespace AutomationTraining_M7.Reporting
             pobjExtent.Flush();
         }
 
-        public void fnAddStepLog(ExtentTest pobjTest, string pstrMessage, string pStatus)
-        {
-            pobjTest.Log(Status.Info, pstrMessage);
-            switch (pStatus.ToUpper())
-            {
-                case "PASS":
-                    pobjTest.Log(Status.Pass, pstrMessage);
-                    break;
-                case "ERROR":
-                    pobjTest.Log(Status.Error, pstrMessage);
-                    break;
-                case "SKIPT":
-                    pobjTest.Log(Status.Skip, pstrMessage);
-                    break;
-                case "WARNING":
-                    pobjTest.Log(Status.Warning, pstrMessage);
-                    break;
-                case "INFO":
-                    pobjTest.Log(Status.Info, pstrMessage);
-                    break;
-                case "FAIL":
-                    pobjTest.Log(Status.Fail, pstrMessage);
-                    break;
-                case "FATAL":
-                    pobjTest.Log(Status.Fatal, pstrMessage);
-                    break;
-                case "DEBUG":
-                    pobjTest.Log(Status.Debug, pstrMessage);
-                    break;
-                default:
-                    pobjTest.Log(Status.Info, pstrMessage);
-                    break;
-            }
-
-        }
-
-        public void fnAddStepLogWithSnapshot(ExtentTest pobjTest, IWebDriver pobjDriver, string pstrMessage, string pstrImageName, string pStatus)
-        {
-            var strImagePath = fnCaptureImage(pobjDriver, pstrImageName);
-            switch (pStatus.ToUpper())
-            {
-                case "PASS":
-                    pobjTest.Pass(pstrMessage, MediaEntityBuilder.CreateScreenCaptureFromPath(strImagePath).Build());
-                    break;
-                case "ERROR":
-                    pobjTest.Error(pstrMessage, MediaEntityBuilder.CreateScreenCaptureFromPath(strImagePath).Build());
-                    break;
-                case "SKIPT":
-                    pobjTest.Skip(pstrMessage, MediaEntityBuilder.CreateScreenCaptureFromPath(strImagePath).Build());
-                    break;
-                case "WARNING":
-                    pobjTest.Warning(pstrMessage, MediaEntityBuilder.CreateScreenCaptureFromPath(strImagePath).Build());
-                    break;
-                case "INFO":
-                    pobjTest.Info(pstrMessage, MediaEntityBuilder.CreateScreenCaptureFromPath(strImagePath).Build());
-                    break;
-                case "FAIL":
-                    pobjTest.Fail(pstrMessage, MediaEntityBuilder.CreateScreenCaptureFromPath(strImagePath).Build());
-                    break;
-                case "FATAL":
-                    pobjTest.Fatal(pstrMessage, MediaEntityBuilder.CreateScreenCaptureFromPath(strImagePath).Build());
-                    break;
-                case "DEBUG":
-                    pobjTest.Debug(pstrMessage, MediaEntityBuilder.CreateScreenCaptureFromPath(strImagePath).Build());
-                    break;
-                default:
-                    pobjTest.Info(pstrMessage, MediaEntityBuilder.CreateScreenCaptureFromPath(strImagePath).Build());
-                    break;
-            }
-        }
 
     }
 }
