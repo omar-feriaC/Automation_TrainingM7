d774d2d1a5b343d4d3f9b0b35507d1f0fdf4818b
dannybeltran <dbeltran@brierley.com>
2019-12-18 11:43:02 -0600

    Completed exercise part 1

--- AutomationTraining_M7/AutomationTraining_M7/Test Cases/Test_PHPTravels.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Test Cases/Test_PHPTravels.cs
@@ -18,24 +18,32 @@ namespace AutomationTraining_M7.Test_Cases
         [Test]
         public void Test_M9Exercise()
         {
-            //Init objects
+            //Init login object(s)
             objTest = objExtent.CreateTest(TestContext.CurrentContext.Test.Name);
             objPHP = new clsPHPTravels_LoginPage(driver);
 
-            //Login Action
+            //Login action(s)
             Assert.AreEqual(true, driver.Title.Contains("Administator Login"), "The Login Page was not loaded correctly.");
             clsPHPTravels_LoginPage.fnEnterEmail(ConfigurationManager.AppSettings.Get("email"));
             clsPHPTravels_LoginPage.fnEnterPassword(ConfigurationManager.AppSettings.Get("password"));
             clsPHPTravels_LoginPage.fnClickLoginButton();
             clsPHPTravels_LoginPage.fnWaitHamburgerMenu();
             Assert.AreEqual(true, driver.Title.Contains("Dashboard"), "The Dashboard was not loaded correctly.");
-            
+
+            //Init dashboaard object(s)
             objDashboard = new clsPHPTravels_Dashboard_Page(driver);
+
+            //Dashboard action(s)
             Console.WriteLine(clsPHPTravels_Dashboard_Page.GetTotalAdminsLink().Text);
+            objTest.Log(Status.Info, clsPHPTravels_Dashboard_Page.GetTotalAdminsLink().Text);
             Console.WriteLine(clsPHPTravels_Dashboard_Page.GetTotalSuppliersLink().Text);
+            objTest.Log(Status.Info, clsPHPTravels_Dashboard_Page.GetTotalSuppliersLink().Text);
             Console.WriteLine(clsPHPTravels_Dashboard_Page.GetTotalCustomersLink().Text);
+            objTest.Log(Status.Info, clsPHPTravels_Dashboard_Page.GetTotalCustomersLink().Text);
             Console.WriteLine(clsPHPTravels_Dashboard_Page.GetTotalGuestsLink().Text);
+            objTest.Log(Status.Info, clsPHPTravels_Dashboard_Page.GetTotalGuestsLink().Text);
             Console.WriteLine(clsPHPTravels_Dashboard_Page.GetTotalBookingsLink().Text);
+            objTest.Log(Status.Info, clsPHPTravels_Dashboard_Page.GetTotalBookingsLink().Text);
         }
     }
 }
