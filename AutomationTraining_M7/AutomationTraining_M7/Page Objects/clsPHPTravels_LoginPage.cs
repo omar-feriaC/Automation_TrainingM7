e97d0779eff1e5d71dfd6ac7df63f79ea2601ba4
dluna86 <47647769+dluna86@users.noreply.github.com>
2019-12-18 07:22:18 -0600

    Start debugging

--- AutomationTraining_M7/AutomationTraining_M7/Page Objects/clsPHPTravels_LoginPage.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Page Objects/clsPHPTravels_LoginPage.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace AutomationTraining_M7.Page_Objects
 {
-    class clsPHPTravels_LoginPage
+    class clsPHPTravels_LoginPage : BaseTest
     {
         /*ATTRIBUTES*/
         public static WebDriverWait _driverWait;
@@ -18,10 +18,10 @@ namespace AutomationTraining_M7.Page_Objects
 
         /*LOCATORS DESCRIPTION*/
         readonly static string STR_EMAIL_TXT = "email";
-        readonly static string STR_EMAIL_TXT2 = "email";
+        //readonly static string STR_EMAIL_TXT2 = "email";
         readonly static string STR_PASSWORD_TXT = "password";
-        readonly static string STRREMEMBERME_LNK = "///label[@class='checkbox']";
-        readonly static string STRREMEMBERME_LNK2 = "//label[@class='checkbox']";
+        readonly static string STRREMEMBERME_LNK = "//input[@value='remember-me']";
+        //readonly static string STRREMEMBERME_LNK2 = "//label[@class='checkbox']";
         readonly static string STR_FORGOTPASS_LNK = "//*[text()='Forget Password']";
         readonly static string STR_LOGIN_BTN = "//span[text()='Login']";
         readonly static string STR_HAMBURGER_BTN = "sidebarCollapse";
@@ -34,11 +34,11 @@ namespace AutomationTraining_M7.Page_Objects
         }
 
         /*OBJECT DEFINITION*/
-        private static IWebElement objEmailTxt = driver.FindElement(By.Name(STR_EMAIL_TXT)); 
-        private static IWebElement objPasswordTxt = driver.FindElement(By.Name(STR_PASSWORD_TXT));
-        private static IWebElement objRememberMeLnk = driver.FindElement(By.XPath(STRREMEMBERME_LNK));
-        private static IWebElement objForgotPassLnk = driver.FindElement(By.XPath(STR_FORGOTPASS_LNK));
-        private static IWebElement objLoginBtn = driver.FindElement(By.XPath(STR_LOGIN_BTN));
+        private static IWebElement objEmailTxt = _objDriver.FindElement(By.Name(STR_EMAIL_TXT)); 
+        private static IWebElement objPasswordTxt = _objDriver.FindElement(By.Name(STR_PASSWORD_TXT));
+        private static IWebElement objRememberMeLnk = _objDriver.FindElement(By.XPath(STRREMEMBERME_LNK));
+        private static IWebElement objForgotPassLnk = _objDriver.FindElement(By.XPath(STR_FORGOTPASS_LNK));
+        private static IWebElement objLoginBtn = _objDriver.FindElement(By.XPath(STR_LOGIN_BTN));
 
 
         /*METHODS/FUNCTIONS*/
@@ -51,8 +51,8 @@ namespace AutomationTraining_M7.Page_Objects
 
         public static void fnEnterEmail(string pstrEmail)
         {
-            clsDriver.fnWaitForElementToExist(By.Name(STR_EMAIL_TXT));
-            clsDriver.fnWaitForElementToBeVisible(By.Name(STR_EMAIL_TXT));
+            //clsDriver.fnWaitForElementToExist(By.Name(STR_EMAIL_TXT));
+            //clsDriver.fnWaitForElementToBeVisible(By.Name(STR_EMAIL_TXT));
             objEmailTxt.Clear();
             objEmailTxt.SendKeys(pstrEmail);
         }
--- AutomationTraining_M7/AutomationTraining_M7/Test Cases/Test_PHPTravels.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Test Cases/Test_PHPTravels.cs
@@ -2,8 +2,11 @@
 using AutomationTraining_M7.Page_Objects;
 using AventStack.ExtentReports;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
