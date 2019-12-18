6a100525aaacfb20b9e9505dd89f996ef4b278c0
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-12-18 10:51:25 -0600

    commit

--- AutomationTraining_M7/AutomationTraining_M7/Test Cases/Test_PHPTravels.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Test Cases/Test_PHPTravels.cs
@@ -28,31 +28,9 @@ namespace AutomationTraining_M7.Test_Cases
             clsPHPTravels_LoginPage.fnClickLoginButton();
             clsPHPTravels_LoginPage.fnWaitHamburgerMenu();
             Assert.AreEqual(true, driver.Title.Contains("Dashboard"), "The Dashboard was not loaded correctly.");
-            objRM.fnAddStepLogWithSnapshot(objTest, driver, "Login Step", "Login123.png", "PASS");
         
         }
 
-        [Test]
-        public void test()
-        {
-            objTest = objExtent.CreateTest(TestContext.CurrentContext.Test.Name);
-            var strNa = @"C:\Tools\GitHub_Review\Automation_TrainingM7\AutomationTraining_M7\AutomationTraining_M7\ExtentReports\Screenshots\Login.png";
-            objTest.Fail("details2",
-                MediaEntityBuilder.CreateScreenCaptureFromPath(strNa).Build());
-            Console.WriteLine();
-        }
-
-
-        [Test]
-        public void test1()
-        {
-            objTest = objExtent.CreateTest(TestContext.CurrentContext.Test.Name);
-            var strNa = @"C:\Tools\GitHub_Review\Automation_TrainingM7\AutomationTraining_M7\AutomationTraining_M7\ExtentReports\Screenshots\Login.png";
-            objTest.Fail("details2",
-                MediaEntityBuilder.CreateScreenCaptureFromPath(strNa).Build());
-            Console.WriteLine();
-            Assert.Fail();
-        }
 
     }
 }
