85238b66a79203303ad026c63d9ec28c2d32151c
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-12-23 09:46:24 -0600

    commit

--- AutomationTraining_M7/AutomationTraining_M7/Program.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Program.cs
@@ -12,34 +12,82 @@ using System.Threading.Tasks;
 
 namespace AutomationTraining_M7
 {
-    class Program : BaseTest
+    class Program
     {
+        //Delegates
+        delegate void LambdasNoParams(); //(1)
+
+
         static void Main(string[] args)
         {
-            clsReportManager objReport = new clsReportManager();
-            objReport.fnReportPath();
+
+            /*L A M B D A */
+
+            //********************************************************
+            // Case 1
+            //********************************************************
+            LambdasWithNoParameter();
+            Console.WriteLine("//********************************************************");
 
 
-            /*
-             * SetUp();
-            LinkedIn_LoginPage objLogin = new LinkedIn_LoginPage(driver);
-            LinkedIn_LoginPage.fnEnterUserName(ConfigurationManager.AppSettings.Get("username"));
-            LinkedIn_LoginPage.fnEnterPassword(ConfigurationManager.AppSettings.Get("password"));
-            LinkedIn_LoginPage.fnClickSignInButton();
-            */
+            //********************************************************
+            // Case 2
+            //********************************************************
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> evenNumbers = list.FindAll(x => (x % 2) == 0);
 
-            /*
-            SalaryEmployee objSalary = new SalaryEmployee(0, "Carlos Ruiz", 100.10);
-            HourlyEmployee objHourly = new HourlyEmployee(1, "Cesar Padilla", 5, 10.5);
-            CommissionEmployee objComm = new CommissionEmployee(2, "Omar Feria", 1000, 500.30);
+            foreach (var num in evenNumbers)
+            {
+                Console.WriteLine("{0} ", num);
+            }
+            Console.WriteLine("//********************************************************");
 
-            objSalary.fnDisplayInfo();
-            objHourly.fnDisplayInfo();
-            objComm.fnDisplayInfo();
-            */
+
+            //********************************************************
+            // Case 3
+            //********************************************************
+            List<int> numbers = new List<int>() { 1,2,3,4,5,6,7,8,9,10 };
+            List<int> oddNumbers = numbers.FindAll(x => x % 2 != 0).ToList();
+            List<int> evenNumbers2 = numbers.FindAll(x => x % 2 == 0).ToList();
+            Console.WriteLine("List of Odd Numbers is");
+            foreach (int ab in oddNumbers)
+            {
+                Console.WriteLine(ab);
+            }
+            Console.WriteLine("List of Even Numbers is");
+            foreach (int ab in evenNumbers2)
+            {
+                Console.WriteLine(ab);
+            }
+            Console.WriteLine("//********************************************************");
+
+
+            //********************************************************
+            // Case 4
+            //********************************************************
+            List<int> numbers2 = new List<int>() { 1,2,3,4,5,6,7,8,9,10 };
+            foreach (int ab in numbers2.Where(x =>
+            {
+                Console.WriteLine("Number is {0}", x);
+                Console.WriteLine("Sqrt of Value is: {0}", Math.Sqrt(x));
+                return x % 3 == 0 || x % 5 == 0;
+            }))
+            {
+                Console.WriteLine("Returned Number is {0}", ab);
+            }
+            Console.WriteLine("//********************************************************");
+
 
             Console.ReadKey();
 
         }
+
+        //(1) No parameters
+        public static void LambdasWithNoParameter()
+        {
+            LambdasNoParams noparams = () => Console.WriteLine(" Hello!!!");
+            noparams();
+        }
     }
+
 }
