9f18120f8b0eb8edef674a74e63ede5018484eb4
Carlos Ruiz <carlos.ruiz@4thsource.com>
2020-01-10 17:14:46 -0600

    Added LINQ Exercises

---
+++ AutomationTraining_M7/AutomationTraining_M7/LINQ_Tests/LINQ_Exercises.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTraining_M7.LINQ_Tests
+{
+    class LINQ_Exercises
+    {
+        public LINQ_Exercises()
+        {
+
+        }
+
+        public void Exercise1()
+        {
+            string[] arr1;
+            int n, i;
+
+            Console.Write("\nLINQ : Print the lenght of the strings in the array : ");
+            Console.Write("\n------------------------------------------------------\n");
+
+            Console.Write("Input number of strings to  store in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+            arr1 = new string[n];
+            Console.Write("\nInput {0} strings for the array  :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Element[{0}] : ", i);
+                arr1[i] = Console.ReadLine();
+            }
+
+            IEnumerable<string> objNew = from m in arr1
+                                         select m;
+
+            foreach (string z in objNew)
+                Console.WriteLine("Item: {0}, characters {1}", z, z.Length);
+
+            Console.ReadLine();
+        }
+
+        public void Exercise2()
+        {
+            string[] dirfiles = Directory.GetFiles("C:/Test");
+            // there are three files in the directory abcd are :
+            // abcd.txt, simple_file.txt and xyz.txt
+
+            Console.Write("\nLINQ : Calculate the Size of File : ");
+            Console.Write("\n------------------------------------\n");
+
+            var avgFsize = dirfiles.Select(file => new FileInfo(file));
+
+            foreach (var file in avgFsize)
+            {
+                Console.WriteLine("The file size is {0} bytes", file.Length);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        public void Exercise3()
+        {
+            //  The first part is Data source.
+            int[] n1 = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.Write("\nBasic structure of LINQ : ");
+            Console.Write("\n---------------------------");
+
+            // The second part is Query creation.
+            // nQuery is an IEnumerable<int>
+            var nQuery =
+                from VrNum in n1
+                where (VrNum % 2) == 0
+                select VrNum;
+
+            // The third part is Query execution.
+
+            Console.Write("\nThe numbers which produce the remainder 0 after divided by 2 are : \n");
+            foreach (int VrNum in nQuery)
+            {
+                Console.Write("{0} ", VrNum);
+            }
+            Console.Write("\n\n");
+        }
+
+        public void Exercise4()
+        {
+            string[] dayWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            Console.Write("\nLINQ : Display the name of the days of a week : ");
+            Console.Write("\n------------------------------------------------\n");
+
+
+            var days = from WeekDay in dayWeek
+                       select WeekDay;
+            foreach (var WeekDay in days)
+            {
+                Console.WriteLine(WeekDay);
+            }
+            Console.WriteLine();
+        }
+
+        public void Exercise5()
+        {
+            var arr1 = new[] { 3, 9, 2, 8, 6, 5 };
+
+            Console.Write("\nLINQ : Print the number and its square from an array: ");
+            Console.Write("\n------------------------------------------------------------------------\n");
+
+            var sqNo = from int Number in arr1
+                       let SqrNo = Number * Number
+                       select new { Number, SqrNo };
+
+            foreach (var a in sqNo)
+                Console.WriteLine(a);
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
--- AutomationTraining_M7/AutomationTraining_M7/Program.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AutomationTraining_M7.Base_Files;
+using AutomationTraining_M7.LINQ_Tests;
 using AutomationTraining_M7.Page_Objects;
 using AutomationTraining_M7.Reporting;
 using NUnit.Framework;
@@ -15,114 +16,15 @@ namespace AutomationTraining_M7
 {
     class Program : BaseTest
     {
-        //Delegates
-        delegate void LambdasNoParams(); //(1)
-
-
         static void Main(string[] args)
         {
-            DataTable myTable;
+            LINQ_Exercises objLINQ = new LINQ_Exercises();
 
-            /* D A T A B A S E*/
-            clsData objData = new clsData();
-            clsLibData objLibData = new clsLibData();
-            objLibData.fnInitConnection();
-
-
-            myTable =  objLibData.fnExecuteQueryData2("select * from UserCredentials");
-            if (myTable != null && myTable.Rows.Count > 0)
-            {
-                //Iterate each row in Table
-                foreach (DataRow row in myTable.Rows)
-                {
-                    if (row["SetValue"].ToString().Trim() == "3")
-                    {
-                        string username = row["UserName"].ToString().Trim();
-                        string password = row["Password"].ToString().Trim();
-                    }
-
-                  
-                }
-            }
-
-
-            objLibData.fnExecuteQueryData("select * from Tbl_Users");
-            objLibData.fnExecuteQueryData("select * from Tbl_Users");
-            objData.fnExecuteQueryData("select * from Tbl_Users");
-
-
-
-
-
-
-            /*L A M B D A */
-
-            //********************************************************
-            // Case 1
-            //********************************************************
-            LambdasWithNoParameter();
-            Console.WriteLine("//********************************************************");
-
-
-            //********************************************************
-            // Case 2
-            //********************************************************
-            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
-            List<int> evenNumbers = list.FindAll(x => (x % 2) == 0);
-
-            foreach (var num in evenNumbers)
-            {
-                Console.WriteLine("{0} ", num);
-            }
-            Console.WriteLine("//********************************************************");
-
-
-            //********************************************************
-            // Case 3
-            //********************************************************
-            List<int> numbers = new List<int>() { 1,2,3,4,5,6,7,8,9,10 };
-            List<int> oddNumbers = numbers.FindAll(x => x % 2 != 0).ToList();
-            List<int> evenNumbers2 = numbers.FindAll(x => x % 2 == 0).ToList();
-            Console.WriteLine("List of Odd Numbers is");
-            foreach (int ab in oddNumbers)
-            {
-                Console.WriteLine(ab);
-            }
-            Console.WriteLine("List of Even Numbers is");
-            foreach (int ab in evenNumbers2)
-            {
-                Console.WriteLine(ab);
-            }
-            Console.WriteLine("//********************************************************");
-
-
-            //********************************************************
-            // Case 4
-            //********************************************************
-            List<int> numbers2 = new List<int>() { 1,2,3,4,5,6,7,8,9,10 };
-            foreach (int ab in numbers2.Where(x =>
-            {
-                Console.WriteLine("Number is {0}", x);
-                Console.WriteLine("Sqrt of Value is: {0}", Math.Sqrt(x));
-                return x % 3 == 0 || x % 5 == 0;
-            }))
-            {
-                Console.WriteLine("Returned Number is {0}", ab);
-            }
-            Console.WriteLine("//********************************************************");
-
-
-            Console.WriteLine("Test");
-
-            Console.ReadKey();
-
-        }
-
-        //(1) No parameters
-        public static void LambdasWithNoParameter()
-        {
-            LambdasNoParams noparams = () => Console.WriteLine(" Hello!!!");
-            noparams();
+            objLINQ.Exercise1();
+            objLINQ.Exercise2();
+            objLINQ.Exercise3();
+            objLINQ.Exercise4();
+            objLINQ.Exercise5();
         }
     }
 
