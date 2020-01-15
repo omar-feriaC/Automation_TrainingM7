998f207314b1ddcc17d7c48098454b7578050e27
dluna86 <47647769+dluna86@users.noreply.github.com>
2020-01-15 17:50:17 -0600

    Ejercicios con Omar

--- AutomationTraining_M7/AutomationTraining_M7/Program.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Program.cs
@@ -11,6 +11,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AutomationTraining_M7
 {
@@ -18,7 +19,7 @@ namespace AutomationTraining_M7
     {
         static void Main(string[] args)
         {
-            LINQ_Exercises objLINQ = new LINQ_Exercises();
+            //LINQ_Exercises objLINQ = new LINQ_Exercises();
 
 
             /*objLINQ.Exercise1();
@@ -30,10 +31,39 @@ namespace AutomationTraining_M7
             //objLINQ.Exercise1();
             //objLINQ.Exercise2();
             //objLINQ.Exercise3();
-            objLINQ.Exercise4();
+            //objLINQ.Exercise4();
             //objLINQ.Exercise5();
 
+            //Console.ReadKey();
+
+
+            //Ejercicio con Omar de crear files
+
+            string strFileName = @"C:\Users\DanielEnriqueLunaRiv\Documents\Automation\LINQ Exercises\TempAuo123.txt";
+            FileInfo objFile = new FileInfo(strFileName);
+
+            using (StreamWriter sw = objFile.CreateText())
+            {
+                sw.WriteLine("Test1");
+                sw.WriteLine("Test2");
+                sw.WriteLine("Test3");
+                sw.WriteLine("Test4");
+                sw.WriteLine("Test5");
+                sw.WriteLine("Test6");
+            }
+
+            //Leer un file e imprimir en la consola lo que hay en el file
+            using (StreamReader sr = File.OpenText(strFileName))
+            {
+                string strValue = "";
+                while ((strValue = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(strValue);
+                }
+            }
+
             Console.ReadKey();
+
         }
     }
 
