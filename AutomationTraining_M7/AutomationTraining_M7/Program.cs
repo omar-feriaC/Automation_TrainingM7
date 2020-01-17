87a183c1f16bed1ed12f74c9b8d86ffe67b58d85
Carlos Paz <carlos.paz@4thsource.com>
2020-01-16 18:14:39 -0600

    Exercise 1 complete

--- AutomationTraining_M7/AutomationTraining_M7/Program.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Program.cs
@@ -16,30 +16,60 @@ namespace AutomationTraining_M7
     class Program
     {
         //Delegates
-        delegate void LambdasNoParams(); //(1)
+ //      delegate void LambdasNoParams(); //(1)
 
 
         static void Main(string[] args)
         {
 
+            /*
             //-------------------------------------------------
-            //Excersie 1
+            //Excersie 1 - WRITE A PROGRAM IN C SHARP TO PRINT THE LENGTH OF A STRING FROM AN ARRAY OF STRINGS.
             //-------------------------------------------------
-            string[] arr1;
-            int n, i;
+
+
+            int n = 0;
+            int i = 0;
+            string total;
 
             Console.Write("\nLINQ : Print the lenght of the strings in the array : ");
             Console.Write("\n------------------------------------------------------\n");
 
             //Write Your code Here
+            Console.Write("Insert the number of strings to write: ");
+            total = Console.ReadLine();
+
+            n = Convert.ToInt32(total);
+
+            string[] arr1 = new string[n];
+
+            Console.WriteLine();
+            Console.WriteLine("Write your strings");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"String {j + 1} = ");
+                arr1[j] = Console.ReadLine();
+            }
+
+            var totalStrings = from words in arr1
+                               select words;
+
+            Console.WriteLine();
+            Console.WriteLine("Your Strings");
+
+            foreach (string x in totalStrings)
+            {
+                Console.WriteLine($"String {i + 1} = {x}, Length: {x.Length}");
+                i++;
+            }
+
             Console.ReadLine();
-
-
-
-
-
+        }
+        */
+        /*
             //-------------------------------------------------
-            //Excersie 2
+            //Excersie 2 - WRITE A PROGRAM IN C SHARP TO PRINT THE SIZE OF A FILE IN BYTES IN A DIRECTORY USING LINQ.
             //-------------------------------------------------
             string[] dirfiles = Directory.GetFiles("C:/Test");
             // there are three files in the directory abcd are :
@@ -51,12 +81,12 @@ namespace AutomationTraining_M7
             //Write Your code
             Console.ReadLine();
 
+        */
 
-
-
+/*
 
             //-------------------------------------------------
-            //Excersie 3
+            //Excersie 3 - WRITE A PROGRAM IN C SHARP TO SHOW HOW THE THREE PARTHS OF A QUERY OPERATION EXECUTE.
             //-------------------------------------------------
             //  The first part is Data source.
             int[] n1 = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
@@ -68,26 +98,27 @@ namespace AutomationTraining_M7
             Console.ReadLine();
 
 
-
+            */
 
 
             //-------------------------------------------------
-            //Excersie 4
+            //Excersie 4 - WRITE A PROGRAM IN C SHARP TO DISPLAY THE NAME OF THE DAYS OF THE WEEK.
             //-------------------------------------------------
             string[] dayWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
             Console.Write("\nLINQ : Display the name of the days of a week : ");
             Console.Write("\n------------------------------------------------\n");
 
-            //Write Your code
-            Console.ReadLine();
+            var day = from x in dayWeek
+                      select x;
 
 
 
 
+            /*
 
             //-------------------------------------------------
-            //Excersie 5
+            //Excersie 5 - WRITE A PROGRAM IN C SHARP TO PRINT THE NUMBER AND ITS SQUARE FROM AN ARRAY.
             //-------------------------------------------------
             var arr3 = new[] { 3, 9, 2, 8, 6, 5 };
 
@@ -97,7 +128,10 @@ namespace AutomationTraining_M7
             //Write Your code
             Console.ReadLine();
 
+    */
         }
+
+   
     }
 
 }
