b499d14148595599cb33769d318a57c9102bfda5
Rodrigo Dominguez Peniche <rodrigo.02.93@gmail.com>
2020-01-17 15:38:21 -0600

    Complete exercise 1, 2, 3, 4 and 5

--- AutomationTraining_M7/AutomationTraining_M7/Program.cs
+++ AutomationTraining_M7/AutomationTraining_M7/Program.cs
@@ -11,38 +11,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AutomationTraining_M7
-{
-    class Program
-    {
+namespace AutomationTraining_M7 {
+    class Program {
         //Delegates
         delegate void LambdasNoParams(); //(1)
 
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
 
             //-------------------------------------------------
             //Excersie 1: print the length of a string from an array of strings
             //-------------------------------------------------
-            string[] arr1;
-            int n, i;
 
-            Console.Write("\nLINQ : Print the lenght of the strings in the array : ");
-            Console.Write("\n------------------------------------------------------\n");
+            Console.Write("\nLINQ : Print the lenght of the strings in the array: ");
+            Console.Write("\n---------------------------------------------------\n");
 
-            
+            string[] arr1 = { "asdqwe", "cdvfbgnhjuh", "tttttttttttttt" };
+            var word = from wlength in arr1 select wlength + " = " + wlength.Length;
+            foreach (var wlength in word)
+            {
+                Console.WriteLine(wlength);
+            }
+
             Console.ReadLine();
-
-
-
-
 
             //-------------------------------------------------
             //Excersie 2: print the size of a file in bytes in a directory using LINQ
             //-------------------------------------------------
-            Console.Write("\nLINQ : Calculate the Size of File : ");
-            Console.Write("\n------------------------------------\n");
+            Console.Write("\nLINQ : Calculate the Size of File: ");
+            Console.Write("\n---------------------------------\n");
 
             long FileSize = 0;
             string[] dirfiles = Directory.GetFiles("C:/Test", "*.txt", SearchOption.TopDirectoryOnly);
@@ -51,50 +48,51 @@ namespace AutomationTraining_M7
 
             Console.ReadLine();
 
-
-
-
-
             //-------------------------------------------------
             //Excersie 3: shows how the three parts of a query operation execute
             //-------------------------------------------------
             //  The first part is Data source.
+
+            Console.Write("\nBasic structure of LINQ: ");
+            Console.Write("\n-----------------------\n");
+
             int[] n1 = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var query = from num in n1
+                        where (num * num) > 10
+                        select num;
+            Console.WriteLine("Numbers from 0 to 9 times itself greater than 10 are:");
+            foreach (int num in query)
+                Console.Write(num + ", ");
 
-            Console.Write("\nBasic structure of LINQ : ");
-            Console.Write("\n---------------------------");
-
-            //Write Your code
             Console.ReadLine();
-
-
-
-
 
             //-------------------------------------------------
             //Excersie 4: display the name of the days of a week
             //-------------------------------------------------
+
+            Console.Write("\nLINQ : Display the name of the days of a week: ");
+            Console.Write("\n---------------------------------------------\n");
+
             string[] dayWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            var day = from weekDay in dayWeek select weekDay;
+            foreach (var weekDay in day)
+                Console.WriteLine(weekDay);
 
-            Console.Write("\nLINQ : Display the name of the days of a week : ");
-            Console.Write("\n------------------------------------------------\n");
-
-            //Write Your code
             Console.ReadLine();
-
-
-
-
 
             //-------------------------------------------------
             //Excersie 5: print the number and its square form an array
             //-------------------------------------------------
-            var arr3 = new[] { 3, 9, 2, 8, 6, 5 };
 
             Console.Write("\nLINQ : Print the number and its square from an array: ");
-            Console.Write("\n------------------------------------------------------------------------\n");
+            Console.Write("\n----------------------------------------------------\n");
+            var arr3 = new[] { 3, 9, 2, 8, 6, 5 };
+            var sqrtQ = from int Number in arr3
+                        let sqrtNum = Number * Number
+                        select new { Number, sqrtNum };
+            foreach (var x in sqrtQ)
+                Console.WriteLine(x);
 
-            //Write Your code
             Console.ReadLine();
 
         }
