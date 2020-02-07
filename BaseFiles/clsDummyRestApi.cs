c193de01d5e6f61535f2c785876f0ef6fc17e14e
Edmundo David Sainz Palacios <edmundo.sainz@4thsource.com>
2020-02-07 17:25:35 -0600

    Add project files.

---
+++ BaseFiles/clsDummyRestApi.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIExercise1.BaseFiles
+{
+    class clsDummyRestApi
+    {
+        internal string post(string name, string salary, string age)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://dummy.restapiexample.com/api/v1/create");
+            request.ContentType = "application/json; charset=UTF-8";
+            request.Method = "POST";
+            // Set some reasonable limits on resources used by this request
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+            // Set credentials to use for this request.
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                string json = "{\"name\":\""+name+"\"," +
+                              "\"salary\":\""+salary+"\"," +
+                              "\"age\":\""+age+"\"}";
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+            }
+            //Request action
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Get the stream associated with the response.
+            Stream receiveStream = response.GetResponseStream();
+
+            // Pipes the stream to a higher level stream reader with the required encoding format. 
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+            string statusMessage = readStream.ReadToEnd();
+            //Console.WriteLine("Response stream received.");
+            Console.WriteLine(statusMessage);
+            
+            string[] splitString = statusMessage.Split(new[] { "\",\"id\":" }, StringSplitOptions.None);
+            
+            response.Close();
+            readStream.Close();
+
+            return splitString[splitString.Length - 1].Substring(0, splitString[splitString.Length - 1].Length-2);
+        }
+
+        internal string delete(string id)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://dummy.restapiexample.com/api/v1/delete/"+id);
+            request.Method = "DELETE";
+            // Set some reasonable limits on resources used by this request
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+            // Set credentials to use for this request.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            //Request action
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Get the stream associated with the response.
+            Stream receiveStream = response.GetResponseStream();
+
+            // Pipes the stream to a higher level stream reader with the required encoding format. 
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+            string statusMessage = readStream.ReadToEnd();
+            //Console.WriteLine("Response stream received.");
+            Console.WriteLine(statusMessage);
+
+            response.Close();
+            readStream.Close();
+            return statusMessage;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using APIExercise1.BaseFiles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APIExercise1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodAPIRequest()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://dummy.restapiexample.com/api/v1/employees/");
+
+            // Set some reasonable limits on resources used by this request
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+            // Set credentials to use for this request.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Content length is {0}", response.ContentLength);
+            Console.WriteLine("Content type is {0}", response.ContentType);
+
+            // Get the stream associated with the response.
+            Stream receiveStream = response.GetResponseStream();
+
+            // Pipes the stream to a higher level stream reader with the required encoding format. 
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+            Console.WriteLine("Response stream received.");
+            Console.WriteLine(readStream.ReadToEnd());
+            response.Close();
+            readStream.Close();
+        }
+        [TestMethod]
+        public void TestMethodAPIPost()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://dummy.restapiexample.com/api/v1/create");
+            request.ContentType = "application/json; charset=UTF-8";
+            request.Method = "POST";
+            // Set some reasonable limits on resources used by this request
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+            // Set credentials to use for this request.
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                string json = "{\"name\":\"Edmund Carat\"," +
+                              "\"salary\":\"123000\"," +
+                              "\"age\":\"33\"}";  
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+            }
+
+            //Request action
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Content length is {0}", response.ContentLength);
+            Console.WriteLine("Content type is {0}", response.ContentType);
+
+            // Get the stream associated with the response.
+            Stream receiveStream = response.GetResponseStream();
+
+            // Pipes the stream to a higher level stream reader with the required encoding format. 
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+            Console.WriteLine("Response stream received.");
+            Console.WriteLine(readStream.ReadToEnd());
+            response.Close();
+            readStream.Close();
+        }
+
+        [TestMethod]
+        public void TestMethodAPIDelete()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://dummy.restapiexample.com/api/v1/delete/2");
+            request.Method = "DELETE";
+            // Set some reasonable limits on resources used by this request
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+            // Set credentials to use for this request.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            //Request action
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Content length is {0}", response.ContentLength);
+            Console.WriteLine("Content type is {0}", response.ContentType);
+
+            // Get the stream associated with the response.
+            Stream receiveStream = response.GetResponseStream();
+
+            // Pipes the stream to a higher level stream reader with the required encoding format. 
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+            Console.WriteLine("Response stream received.");
+            Console.WriteLine(readStream.ReadToEnd());
+            response.Close();
+            readStream.Close();
+        }
+        [TestMethod]
+        public void TestMethodClassApi()
+        {
+            var myClassDummyApi = new clsDummyRestApi();
+            string id = myClassDummyApi.post("Edmund Carat", "123000", "33");
+            Console.WriteLine("Created employee id : {0}",id);
+            
+            string delStatMessage = myClassDummyApi.delete(id);
+            Console.WriteLine(delStatMessage);
+        }
+    }
+}
