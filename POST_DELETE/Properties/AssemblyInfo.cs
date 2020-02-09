4f4d226c037a5e5040c824db6902e1d544d3a366
lesperon <luis.esperon@4thsource.com>
2020-02-08 21:54:57 -0600

    Add project files.

---
+++ POST_DELETE/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("POST_DELETE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("POST_DELETE")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00f35ea2-fa79-4676-afa3-f43606cfb63d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ POST_DELETE/UnitTest1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace POST_DELETE
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        public class Employee
+        {
+            public string status { get; set; }
+            public Data data { get; set; }
+    
+        }
+
+
+        public class Data
+        {
+
+            public string  name { get; set; }
+            public int salary { get; set; }
+
+            public int age { get; set; }
+
+            public int id { get; set; }
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+            var url = "http://dummy.restapiexample.com/api/v1/create";
+            WebRequest post = HttpWebRequest.Create(url);
+
+
+            post.ContentType = "application/json; charset=utf-8";
+            post.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(post.GetRequestStream()))
+            {
+                string json = "{\"name\":\"luis\",\"salary\":\"500\",\"age\":\"36\"}";
+
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                Console.WriteLine("POST Sent: " + json);
+            }
+
+
+
+
+            WebResponse response = post.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string responseText = reader.ReadToEnd();
+
+            Console.WriteLine("Response Received: " + responseText);
+
+
+
+            Employee json2 = JsonConvert.DeserializeObject<Employee>(responseText);
+
+
+            Console.WriteLine($"Delete Request Sent: http://dummy.restapiexample.com/api/v1/delete/{json2.data.id}");
+
+
+
+            var url2 = $"http://dummy.restapiexample.com/api/v1/delete/{json2.data.id}";
+            WebRequest post2 = HttpWebRequest.Create(url2);
+            post2.Method = "DELETE";
+
+            using (var streamWriter2 = new StreamWriter(post2.GetRequestStream()))
+            {
+
+                WebResponse response2 = post2.GetResponse();
+
+                StreamReader reader2 = new StreamReader(response2.GetResponseStream());
+
+                string responseText2 = reader2.ReadToEnd();
+
+                Console.WriteLine($"Response Received when trying to delete Employee: {json2.data.name} with id: {json2.data.id} \n" + responseText2);
+
+
+
+            }
+
+
+
+        }
+    }
+}
