3acfef01ac15f783311d115aed83d894752b957c
jcouoh <47648044+jcouoh@users.noreply.github.com>
2020-02-17 11:26:54 -0600

    Part 1 ready.

---
+++ AutomationTraining_M7/AutomationTraining_M7/M7_Exercise/Jaime_Couoh_M7_Exercise/2D_Shape.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTraining_M7.M7_Exercise.Jaime_Couoh_M7_Exercise
+{
+    class _2D_Shape : IShape
+    {
+        //Attributes from IShape
+        public double dblArea { get; set; }
+        public double dblPerimtr { get; set; }
+
+        //Attribute for 2D_Shape
+        public string strName { get; set; }
+
+        //Constructor
+        public _2D_Shape() { }
+
+        //Constructor to initialized with 0
+        public _2D_Shape (string pName)
+        {
+            dblArea = 0;
+            dblPerimtr = 0;
+            strName = pName;
+         }
+
+        //Constructor to provide values
+        public _2D_Shape (double pArea, double pPerimtr, string pName)
+        {
+            dblArea = pArea;
+            dblPerimtr = pPerimtr;
+            strName = pName;
+        }
+
+        //IShape Methods
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Info for 2D_Shape");
+            Console.WriteLine("Name is" + strName);
+            Console.WriteLine("Area is" + dblArea);
+            Console.WriteLine("Perimeter is" + dblPerimtr);
+        }
+
+    }
+}
---
+++ AutomationTraining_M7/AutomationTraining_M7/M7_Exercise/Jaime_Couoh_M7_Exercise/3D_Shape.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTraining_M7.M7_Exercise.Jaime_Couoh_M7_Exercise
+{
+    class _3D_Shape : IShape
+    {
+        //Attributes from IShape
+        public double dblArea { get; set; }
+        public double dblPerimtr { get; set; }
+
+        //Attribute for 3D_Shape
+        public string strName { get; set; }
+        public double dblVolume { get; set; }
+
+        //Constructor
+        public _3D_Shape() { }
+
+        //Constructor to initialized
+        public _3D_Shape(string pName)
+        {
+            dblArea = 0;
+            dblPerimtr = 0;
+            dblVolume = 0;
+            strName = pName;
+        }
+
+        //Constructor for values
+        public _3D_Shape(double pArea, double pPerimtr, double pVolume, string pName)
+        {
+            dblArea = pArea;
+            dblPerimtr = pPerimtr;
+            dblVolume = pVolume;
+            strName = pName;
+
+        }
+
+        //IShape Methods
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Info for 3D_Shape");
+            Console.WriteLine("Name is" + strName);
+            Console.WriteLine("Area is" + dblArea);
+            Console.WriteLine("Perimeter is" + dblPerimtr);
+            Console.WriteLine("Volume is" + dblPerimtr);
+        }
+
+        public double fnCalculateVolume ()
+        {
+            return dblVolume;
+        }
+    }
+}
