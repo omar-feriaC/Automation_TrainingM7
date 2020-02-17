952ce3eafb1cdbcd7f1eb864e7f7b2f852644780
jcouoh <47648044+jcouoh@users.noreply.github.com>
2020-02-17 10:32:19 -0600

    Interfaz Lista

---
+++ AutomationTraining_M7/AutomationTraining_M7/M7_Exercise/Jaime_Couoh_M7_Exercise/IShape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTraining_M7.M7_Exercise.Jaime_Couoh_M7_Exercise
+{
+    interface IShape
+    {
+        //Attributes
+        double dblArea { get; set; }
+        double dblPerimtr { get; set; }
+
+        //Methods
+        void DisplayInfo();
+
+    }
+}
